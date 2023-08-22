361a25f29ae81dafd511b6cb573a695fce191e66
Sklup55 <ksundararajan@skillup.tech>
2023-08-22 06:01:42 -0400

    2nd commit in React app

---
+++ server/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+const http = require('http');
+const connectToMongo = require('./db');
+const app = express();
+
+
+app.set('view engine','ejs')
+app.use(express.static('public'))
+
+const PORT = process.env.PORT || 8181;
+const doctorsDetail=require('./models/doctors')
+
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Connect to MongoDB
+connectToMongo();
+
+// Routes
+app.use('/api/auth', require('./routes/auth'));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/doctors',async(req,res)=>{
+  try{
+      const getDoctors=await doctorsDetail.find({});
+      res.send(getDoctors)
+  }catch(e){
+      console.log(e);
+  }
+})
+app.get('/doctorimage',(req,res)=>{
+  res.render('index')
+})
+
+
+  // Start the server
+app.listen(PORT, () => {
+console.log(`Server is running on port http://localhost:${PORT}`);
+});
