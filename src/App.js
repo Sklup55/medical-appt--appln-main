beebc81ce4f6e4fe048ed44a1f48490f73639a52
Sklup55 <ksundararajan@skillup.tech>
2023-08-24 03:17:44 -0400

    week 3 4th lab completed & week 3 fully done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Login from './Components/Login/Login';
 import SignUp from './Components/Sign_Up/Sign_Up';
 import Navbar from './Components/Navbar/Navbar';
 import InstantConsultation from './Components/InstantConsultation/InstantConsultation';
+import FindDoctorSearch from './Components/FindDoctorSearch/FindDoctorSearch';
+import DoctorBook from './Components/DoctorCard/DoctorBook';
 
 
 function App() {
@@ -14,9 +16,17 @@ function App() {
       <Routes>
 
         <Route path="/signup" element={<SignUp />} /> {/* Define the route for Sign Up */}
+
         <Route path="/login" element={<Login />} /> {/* Define the route for Login */}
 
         <Route path="/instant-consultation" element={<InstantConsultation/>} /> {/* Define the route for Login */}
+
+        <Route path='/finddoctor' element={<FindDoctorSearch/>}/>
+        
+        <Route path="/search/doctors" element={<DoctorBook/>} />
+
+
+
 
 
       </Routes>
